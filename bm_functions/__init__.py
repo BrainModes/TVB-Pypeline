5bdb30d52b439e70c31d644193c3669858b7697d
Simon R <srothmei@mailbox.tu-berlin.de>
2016-03-04 10:52:58 +0100

    Debugging the fmRI

--- bm_functions/__init__.py
+++ bm_functions/__init__.py
@@ -3,5 +3,4 @@ from .compFC import compute_functional_connectivity
 from .compSC_row import compute_connectivity_row
 from .generateMasks import generate_masks
 from .connectivity2TVBFS import connectivity_2_tvb_fs
-from .default_feat_config import gen_default_feat_config
 from .util_functions import *
